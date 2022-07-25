87b8c720ce12c180da04a31ec4a84dd7b30a9b4f
Yash Herekar <46808309+1337encrypted@users.noreply.github.com>
2022-07-25 15:56:06 +0530

    Update testmotor.ino

--- testmotor.ino
+++ testmotor.ino
@@ -19,7 +19,6 @@ void front()
   digitalWrite(in2,LOW);
   analogWrite(pwma,Speed);
 }
-
 void back()
 {
   digitalWrite(in1,LOW);
@@ -32,83 +31,83 @@ void Stp()
   digitalWrite(in2,LOW);
   analogWrite(pwma,0);
 }
+void loop()
+{
+    if (Serial.available() > 0)
+    {  
+      
+      key=Serial.read();
 
-void loop() {
-  // put your main code here, to run repeatedly:
-if (Serial.available() > 0)
-  {  
-    key=Serial.read();
-    
-    if(key == 'F')
-    {
-      front();
-      Serial.println("Foreward: "+Speed);
-    }
-    else if(key == 'B')
-    {
-      back();
-      Serial.println("Backward: "+Speed);
-    }
-    else if(key == '0')
-    {
-      Speed = 115;
-      //Serial.println("Speed = 115");
-    }
-    else if(key == '1')
-    {
-      Speed = 130;
-      //Serial.println("Speed = 130");
-    }
-    else if(key == '2')
-    {
-      Speed = 143;
-      //Serial.println("Speed = 143");
-    }
-    else if(key == '3')
-    {
-      Speed = 157;
-      //Serial.println("Speed = 157");
-    }
-    else if(key == '4')
-    {
-      Speed = 170;
-      //Serial.println("Speed = 170");
-    }
-    else if(key == '5')
-    {
-      Speed = 185;
-      //Serial.println("Speed = 185");
-    }
-    else if(key == '6')
-    {
-      Speed = 200;
-      //Serial.println("Speed = 200");
-    }
-    else if(key == '7')
-    {
-      Speed = 213;
-      //Serial.println("Speed = 213");
-    }
-    else if(key == '8')
-    {
-      Speed = 227;
-      //Serial.println("Speed = 227");
-    }
-    else if(key == '9')
-    {
-      Speed = 240;
-      //Serial.println("Speed = 240");
-    }
-    else if(key == 'q')
-    {
-      Speed = 255;
-      //Serial.println("Speed = 255");
-    }
-    else
-    {
-      Stp();
-      Serial.println("Stop: "+0); 
-    }
-    delay(5);
+      if(key == 'F')
+      {
+        front();
+        Serial.println("Foreward: "+Speed);
+      }
+      else if(key == 'B')
+      {
+        back();
+        Serial.println("Backward: "+Speed);
+      }
+      else if(key == '0')
+      {
+        Speed = 115;
+        //Serial.println("Speed = 115");
+      }
+      else if(key == '1')
+      {
+        Speed = 130;
+        //Serial.println("Speed = 130");
+      }
+      else if(key == '2')
+      {
+        Speed = 143;
+        //Serial.println("Speed = 143");
+      }
+      else if(key == '3')
+      {
+        Speed = 157;
+        //Serial.println("Speed = 157");
+      }
+      else if(key == '4')
+      {
+        Speed = 170;
+        //Serial.println("Speed = 170");
+      }
+      else if(key == '5')
+      {
+        Speed = 185;
+        //Serial.println("Speed = 185");
+      }
+      else if(key == '6')
+      {
+        Speed = 200;
+        //Serial.println("Speed = 200");
+      }
+      else if(key == '7')
+      {
+        Speed = 213;
+        //Serial.println("Speed = 213");
+      }
+      else if(key == '8')
+      {
+        Speed = 227;
+        //Serial.println("Speed = 227");
+      }
+      else if(key == '9')
+      {
+        Speed = 240;
+        //Serial.println("Speed = 240");
+      }
+      else if(key == 'q')
+      {
+        Speed = 255;
+        //Serial.println("Speed = 255");
+      }
+      else
+      {
+        Stp();
+        Serial.println("Stop: "+0); 
+      }
+      delay(5);
   }
 }
